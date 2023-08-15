473a7b8f50a613d7b2b7526820144a905575afd8
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2023-08-15 18:34:57 -0300

    correcciones de textos

--- components/layout/boton.tsx
+++ components/layout/boton.tsx
@@ -9,7 +9,7 @@ function Button({ children, className = "w-max" }: ButtonProps) {
   return (
     <button
       className={
-        "px-4 py-2 text-white bg-gradient-to-r shadow-md shadow-blue/20 hover:shadow-xl hover:shadow-blue/60 from-pink to-blue hover:-translate-y-1 rounded-md transition-all delay-75" +
+        "px-4 py-2 text-white bg-gradient-to-r hover:bg-gradient-to-l shadow-md shadow-blue/20 hover:shadow-xl hover:shadow-blue/60 from-pink to-blue hover:-translate-y-1 rounded-md transition-all delay-75" +
         `${className}`
       }
     >
